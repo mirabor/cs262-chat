6ef28d3622b83ccb27adc5451f5f55c5387a48f5
Sam Mucyo <90157786+Sam-Mucyo@users.noreply.github.com>
2025-02-11 10:09:11 -0500

    remove skipping protocol tests for `CustomProtocol`

--- tests/protocol/test_custom_protocol.py
+++ tests/protocol/test_custom_protocol.py
@@ -9,11 +9,3 @@ class TestCustomProtocol(BaseProtocolTest):
     @pytest.fixture
     def protocol(self):
         return CustomProtocol()
-
-    @pytest.mark.skip(reason="CustomProtocol is not implemented yet")
-    def test_serialization(self, protocol, valid_message):
-        super().test_serialization(protocol, valid_message)
-
-    @pytest.mark.skip(reason="CustomProtocol is not implemented yet")
-    def test_deserialization(self, protocol, valid_message):
-        super().test_deserialization(protocol, valid_message)
